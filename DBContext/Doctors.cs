a90a2a60f7edb88d573a577a51faa86bc0543eb2
Predartplus <predartplus@outlook.com>
2024-07-28 22:36:43 +0530

    AppLOV added

---
+++ DBContext/Doctors.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DartPlusAPI.DBContext
+{
+    public class Doctors: Base
+    {
+        [Key]
+        public Guid DoctorID { get; set; }
+        public string DoctorName { get; set; }
+        public Guid QualificationID { get; set; }
+        public string MemberShip { get; set; }
+        public string Registration { get; set; }
+        public string DoctorEmail { get; set; }
+        public string DoctorMobile { get; set; }
+    }
+}
---
+++ IServices/IAppLOVService.cs
@@ -0,0 +1,15 @@
+﻿using DartPlusAPI.DBContext;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DartPlusAPI.IServices
+{
+    public interface IAppLOVService
+    {
+        Task<ActionResult<object>> GetAppLOVs();
+        Task<ActionResult<object>> GetAppLOV(Guid id);
+        Task<ActionResult<object>> GetAppLOV(string? type);
+        Task<ActionResult<object>> AddAppLOV(AppLOV AppLOV);
+        Task<ActionResult<object>> UpdateAppLOVStatus(Guid id,string Updated,bool IsActive);
+        Task<ActionResult<object>> DeleteAppLOV(Guid id);
+    }
+}
